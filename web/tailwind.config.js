6e23fae9578980b0ad0bf41d5cb314c56b1f74a7
minhman <phminhman2501@gmail.com>
2023-09-08 11:07:33 +0700

    fix(page): delete the default nextjs page

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,10 +7,94 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			fontSize: {
+				heading: '2.8rem',
+				'heading-1': '2.4rem',
+				'heading-2': '2.2rem',
+				'heading-3': '1.8rem',
+				'heading-4': '1.6rem',
+				'heading-allcap-lg': '1.8rem',
+				'heading-allcap-md': '1.6rem',
+				'body-xl': '2.1rem',
+				'body-lg': '1.8rem',
+				'body-md': '1.6rem',
+				'body-sm': '1.4rem',
+				'body-xsm': '1.2rem',
+			},
+			spacing: {
+				compact: '0.8rem',
+				cozy: '1.6rem',
+				common: '2rem',
+				comfortable: '3.2rem',
+			},
+			backgroundColor: {
+				primary: {
+					580: {
+						DEFAULT: '#BFCBA8',
+						80: '#CCD5B9',
+						60: '#D9E0CB',
+						40: '#E5EADC',
+						20: '#F2F5EE',
+					},
+					625: {
+						DEFAULT: '#5B8A72',
+						80: '#78A68E',
+						60: '#99BCAA',
+						40: '#bbd2c7',
+						20: '#dde9e3',
+					},
+					5555: {
+						DEFAULT: '#56776C',
+						80: '#71998c',
+						60: '#95b2a8',
+						40: '#b8ccc5',
+						20: '#dce5e2',
+					},
+					418: {
+						DEFAULT: '#464F41',
+						80: '#6a7762',
+						60: '#8e9c87',
+						40: '#b4bdaf',
+						20: '#d9ded7',
+					},
+				},
+				secondary: {
+					376: '#8DBF2E',
+					186: '#CC2647',
+					1495: '#FF8B28',
+					116: '#F2CE1B',
+					100: '#F6EB69',
+				},
+				'action-link': '#01a049',
+				neutral: {
+					gray: {
+						1: '#FFFFFF',
+						2: '#F8F8F8',
+						3: '#eeeeee',
+						4: '#dcdcdc',
+						5: '#cecece',
+						6: '#BABABA',
+						7: '#A0A0A0',
+						8: '#999999',
+						9: '#666666',
+						10: '#333333',
+					},
+				},
+				status: {
+					error: {
+						DEFAULT: '#E90000',
+						mid: '#FF807F',
+						light: '#ffe7e7',
+					},
+					success: {
+						DEFAULT: '#53AF3B',
+						light: '#DDEFD8',
+					},
+					warning: {
+						DEFAULT: '#fb9716',
+						light: '#fdeede',
+					},
+				},
 			},
 		},
 	},
