ed18f458e04aa9dd765e3663ed8786cb954bdce2
minhman <phminhman2501@gmail.com>
2023-09-10 14:03:47 +0700

    feat: implement image to the layout, change max width for extra large screen breakpoint

--- web/app/(routes)/(authentication)/layout.tsx
+++ web/app/(routes)/(authentication)/layout.tsx
@@ -1,11 +1,22 @@
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+	title: 'Create An Account',
+	description: 'Be a member by creating an account',
+}
 export default function AuthenticationLayout({ children }: { children: ReactNode }) {
 	return (
 		<div className='flex-center h-screen w-screen bg-primary-580-20'>
-			<div className='container m-auto grid h-[90%] max-h-full grid-cols-2 overflow-hidden rounded-xl bg-white shadow-38'>
+			<div className='container my-auto grid h-[95%] max-h-full grid-cols-2 overflow-hidden rounded-xl bg-white shadow-38'>
 				<div className='flex-center max-h-full p-[3.2rem]'>{children}</div>
-				<div className=''></div>
+				<div className=''>
+					<img
+						className='object-fill'
+						src='https://static.vecteezy.com/system/resources/previews/018/815/357/original/green-tropical-forest-background-monstera-leaves-palm-leaves-branches-exotic-plants-background-for-banner-template-decor-postcard-abstract-foliage-and-botanical-wallpaper-vector.jpg'
+						alt=''
+					/>
+				</div>
 			</div>
 		</div>
 	)
--- web/app/(routes)/(authentication)/register/page.tsx
+++ web/app/(routes)/(authentication)/register/page.tsx
@@ -3,7 +3,18 @@ import RegisterForm from './RegisterForm'
 export default function RegisterPage() {
 	return (
 		<>
-			<RegisterForm />
+			<div className='flex-col-start gap-common'>
+				<div>
+					<span className='mb-cozy block text-body-xl'>
+						Welcome to <span className='text-heading font-bold'>GreenDeco</span> 👋
+					</span>
+					<div className='flex-col-start gap-compact'>
+						<h1>Create An Account</h1>
+						<p className='text-body-md'>Become one of the plant lovers now!</p>
+					</div>
+				</div>
+				<RegisterForm />
+			</div>
 		</>
 	)
 }
