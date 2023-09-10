3611fc0e135e2c00180bdd2fa6ccb9cff368d0a4
minhman <phminhman2501@gmail.com>
2023-09-10 09:06:07 +0700

    build: enable tailwindcss reset css, add flex utilites

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
-	corePlugins: {
-		preflight: false,
-	},
 	important: '#__next',
 	theme: {
 		extend: {
