6a3c56fa669e3cd8e0c7634147fcd00829f87fa1
minhman <phminhman2501@gmail.com>
2023-09-11 16:11:28 +0700

    feat(input): enable outline when focus

--- web/app/_components/form/Input.tsx
+++ web/app/_components/form/Input.tsx
@@ -1,18 +1,23 @@
 'use client'
 import * as React from 'react'
 import { Input as BaseInput, Input, InputProps } from '@mui/base/Input'
+import clsx from 'clsx'
 
 const CustomInput = React.forwardRef(function CustomInput(
 	props: InputProps,
 	ref: React.ForwardedRef<HTMLInputElement>,
 ) {
+	const { className } = props
 	return (
 		<BaseInput
 			{...props}
 			ref={ref}
 			slotProps={{
+				root: {
+					className: clsx(['base-input', className]),
+				},
 				input: {
-					className: 'base-input',
+					className: 'w-full outline-none',
 				},
 			}}
 		/>
