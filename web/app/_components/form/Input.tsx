b2f1517241aec853434aa9da2a511344e91e1706
minhman <phminhman2501@gmail.com>
2023-09-12 10:29:07 +0700

    feat: change input padding - implement start icon to input

--- web/app/_components/form/Input.tsx
+++ web/app/_components/form/Input.tsx
@@ -2,16 +2,19 @@
 import * as React from 'react'
 import { Input as BaseInput, InputProps } from '@mui/base/Input'
 import clsx from 'clsx'
+import { log } from 'console'
 
+//NOTE: Input
 const CustomInput = React.forwardRef(function CustomInput(
 	props: InputProps,
 	ref: React.ForwardedRef<HTMLInputElement>,
 ) {
-	const { className } = props
+	const { className, type } = props
 	return (
 		<BaseInput
 			{...props}
 			ref={ref}
+			startAdornment={type && renderInputType(type)}
 			slotProps={{
 				root: {
 					className: clsx(['base-input', className], {
@@ -27,21 +30,26 @@ const CustomInput = React.forwardRef(function CustomInput(
 	)
 })
 
-const Input = React.forwardRef(function Input(
-	props: InputProps,
-	ref: React.ForwardedRef<HTMLInputElement>,
-) {
-	const renderInputType = (type: React.HTMLInputTypeAttribute) => {
-		return <span>{type}</span>
-	}
-	return (
-		<CustomInput
-			{...props}
-			ref={ref}
-			startAdornment={props.type && renderInputType(props.type)}
-		/>
-	)
-})
+//NOTE: Input startAdornment
+const renderInputType = (type: React.HTMLInputTypeAttribute) => {
+	const startIcon = StartIconArray.find((startIcon) => startIcon['type'] == type)
+	if (startIcon) return startIcon.icon
+}
+
+type StartIconType = {
+	type: 'email' | 'password'
+	icon: React.ReactNode
+}
+
+const StartIconArray: StartIconType[] = [
+	{
+		type: 'email',
+		icon: <span>Email</span>,
+	},
+	{
+		type: 'password',
+		icon: <span>Password</span>,
+	},
+]
 
 export default CustomInput
-export { Input }
