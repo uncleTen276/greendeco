1d6f45472fc7b5e856a8c44a8ffc045a42946516
minhman <phminhman2501@gmail.com>
2023-09-12 15:17:52 +0700

    feat(button): develop button CSS

--- web/app/_components/Button.tsx
+++ web/app/_components/Button.tsx
@@ -6,15 +6,12 @@ const Button = React.forwardRef(function Button(
 	props: ButtonProps,
 	ref: React.ForwardedRef<HTMLButtonElement>,
 ) {
-	const { className, type } = props
+	const { className, ...rest } = props
 	return (
 		<BaseButton
-			{...props}
+			{...rest}
 			ref={ref}
-			className={clsx(
-				'cursor-pointer rounded-lg border-none bg-violet-500 px-4 py-2 font-sans text-sm font-semibold text-white disabled:cursor-not-allowed disabled:opacity-50',
-				className,
-			)}
+			className={clsx('btn', className)}
 		/>
 	)
 })
