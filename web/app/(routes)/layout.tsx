6c89f715d8d82fe50b5b2684dd1efb714bad2ddc
minhman <phminhman2501@gmail.com>
2023-09-19 11:28:56 +0700

    feat: add react-toastify, implement to the main layout

--- web/app/(routes)/layout.tsx
+++ web/app/(routes)/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
+import 'react-toastify/dist/ReactToastify.css'
 import type { Metadata } from 'next'
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -11,7 +13,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='en'>
 			<body>
-				<div id='__next'>{children}</div>
+				<div id='__next'>
+					<ToastContainer className='w-auto' />
+					{children}
+				</div>
 			</body>
 		</html>
 	)
