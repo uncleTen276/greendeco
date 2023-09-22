fcc2007a7958cebb1b2b6811942e54a614ce4924
sekke276 <tringuyen2762001@gmail.com>
2023-09-22 10:13:21 +0700

    add cors

--- api/vercel.go
+++ api/vercel.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sekke276/greendeco.git/pkg/configs"
 	"github.com/sekke276/greendeco.git/pkg/routes"
 	"github.com/sekke276/greendeco.git/platform/database"
@@ -43,6 +45,8 @@ func handler() http.HandlerFunc {
 	}
 
 	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(logger.New())
 	app.Get("/hello", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
